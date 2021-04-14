882c21bc0d3ce5c0acbe1d8e5cfd6467ff0abbc8
Tim <ray.timothy@knights.ucf.edu>
2021-04-14 12:32:16 -0400

    transition script on hub player

--- Assets/Scripts/HubNavigation.cs
+++ Assets/Scripts/HubNavigation.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DoorTransition : MonoBehaviour
+public class HubNavigation : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1.5f;
 
     void OnTriggerEnter(Collider other)
     {
+        //transition to forest level
         if(other.tag == "Door1")
         {
             StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
         }
 
+        //transition to town level
         if(other.tag == "Door2")
         {
             StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 2));
         }
 
+        //transition to scary level
         if(other.tag == "Door3")
         {
             StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 3));
         }
-    }
 
+        //Hub's "death pit"
+        if(other.tag =="HubPit")
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+    }
 
 
     IEnumerator LoadLevel(int levelIndex)
