d51763b2a86526fc74696fbad70bf1b2795a4fee
Tim <ray.timothy@knights.ucf.edu>
2021-04-13 20:23:21 -0400

    hub level laid out

---
+++ Assets/Scripts/DoorTransition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTransition : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1.5f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Door1")
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+
+        if(other.tag == "Door2")
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 2));
+        }
+
+        if(other.tag == "Door3")
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 3));
+        }
+    }
+
+
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //play anim
+        transition.SetTrigger("Start");
+        //wait
+        yield return new WaitForSeconds(transitionTime);
+        //load next scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Assets/Scripts/HubResetPos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubResetPos : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="HubPit")
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
